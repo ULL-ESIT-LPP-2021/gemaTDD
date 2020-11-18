bf3e24ecb26e81dcc604ecf3ea7c33df161a7ce0
Coromoto <cleon@ull.edu.es>
2020-11-18 19:36:55 +0000

    Creada la clase Recta

--- lib/gema/recta.rb
+++ lib/gema/recta.rb
@@ -1,2 +1,3 @@
 class Recta
 end
+
--- spec/recta_spec.rb
+++ spec/recta_spec.rb
@@ -1,5 +1,8 @@
 
 RSpec.describe Gema do
   describe Recta do    # Agrupa pruebas
+	 it "Se debe poder instanciar una recta" do  # Describe una prueba 
+		 expect(Recta.new).not_to eq(nil)
+	 end
   end
 end
